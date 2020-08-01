b7911633d0c68787a54501f603efbd0137fc17b0
John Bucknam <>
2020-08-01 19:16:34 -0400

    Added docstring and comments to InclusionMap
    Made the following changes:
    
    	1. Updated InclusionMap with comments and docstrings.
    	2. Updated InclusionMap._generated_map by remove key list.

--- Map/InclusionMap.py
+++ Map/InclusionMap.py
@@ -2,9 +2,19 @@ from Map import TreeNode
 
 
 class InclusionMap(object):
+    """The mapping used to organize and initialize TreeNodes in relation
+    to each other.
+
+    Args:
+        numbers (list[int]): The numbers used in the Numbers Round.
+
+    """
 
     def __init__(self, numbers):
+        # The main dict map used to store each TreeNode
         self.map = {}
+
+        # Initializing the map values and TreeNodes
         self._generate_map([i for i in range(len(numbers))])
         self._generate_refs()
 
@@ -15,31 +25,57 @@ class InclusionMap(object):
             numbers (list[int]): The set of numbers that can be used.
             s (tuple): The string that is used as the key for the dictionary
             idx (int): The starting index for reading in numbers
-        """
-        if idx == len(numbers):
-            return []
 
-        keys = []
+        """
+        # If the index is the at the end of the list, exit function
+        if idx == len(numbers):
+            return
+
         for i in range(idx, len(numbers)):
             num = numbers[i]
+
+            # For each number, create a new tuple key and new TreeNode object
             s1 = s + (num,)
             self.map[s1] = TreeNode(s1)
-            keys.append(s1)
-            keys += self._generate_map(numbers, s1, i+1)
-        return keys
+
+            # Create new keys and TreeNodes based on the current key
+            self._generate_map(numbers, s1, i+1)
 
     def _generate_refs(self):
+        """Adds references from one TreeNode to another TreeNode
+
+        This method constructs a structure between different TreeNodes with the following properties:
+
+            1. A TreeNode with a key of length n will only reference to other TreeNodes with a key of length n+1.
+            2. A TreeNode with a key of set k will only reference other TreeNodes that are a superset of set k.
+
+        Example:
+
+            TreeNode of key (0, 1, 3) connects to TreeNodes of keys (0, 1, 2, 3), (0, 1, 3, 4), (0, 1, 3, 5), etc.
+            TreeNode of key (0) connects to TreeNodes of keys (0, 1), (0, 2), etc.
+            TreeNode of key (0, 1, 2, 3, ..., n) of max length n will connect to no TreeNodes.
+
+        """
+        # Obtain list of keys and sort based on length
         keys = list(self.map.keys())
         keys.sort(key=lambda x: len(x))
 
+        # Add references to each TreeNode in the map
         for key in keys:
             for ref_key in keys:
+                # Keys are sorted by length, we have passed all the keys we care about
                 if len(ref_key) > (len(key) + 1):
                     break
+
+                # Reference key isn't proper length, skip reference key
                 if len(ref_key) != (len(key) + 1):
                     continue
+
+                # Reference key isn't superset of current key, skip reference key
                 if not all(item in ref_key for item in key):
                     continue
+
+                # Properties validated; adding reference to specific TreeNode
                 self.map[key].node_forest.append(self.map[ref_key])
 
 
