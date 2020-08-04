80510cd688edefb45da27eff16105f87c482fec0
John Bucknam <unknown>
2020-08-04 16:17:20 -0400

    Added Best Answer to NumbersRoundSolver
    If there is no answer that exists, the NumbersRoundSolver now yields the
    closest answer.
    
    Float comparisons were also changed to use math.isclose function.

--- NumbersRoundSolver.py
+++ NumbersRoundSolver.py
@@ -1,8 +1,8 @@
 from Node import NumberTree
 from Map import InclusionMap
 from collections import deque
-import sys
 import time
+import math
 import random
 
 
@@ -61,12 +61,16 @@ class NumbersRoundSolver(object):
             tree_map.add(tree)
             tree_queue.append(tree)
 
+        best = tree_queue[0]
+
         # Main loop
         while len(tree_queue) != 0:
             left_subtree = tree_queue.popleft()
             # If tree is a solution, yield the answer
-            if abs(left_subtree.value - self.goal) < sys.float_info.epsilon:
+            if math.isclose(left_subtree.value, self.goal):
                 yield left_subtree
+            if abs(best.value - self.goal) > abs(left_subtree.value - self.goal):
+                best = left_subtree
 
             # Find all possible selection with left subtree
             right_selection = tree_map.get_trees(left_subtree.key)
@@ -80,8 +84,12 @@ class NumbersRoundSolver(object):
                 self._add_tree(tree_map, tree_queue, left_subtree, right_subtree, "-")
 
                 # Add division tree if right value isn't 0
-                if abs(right_subtree.value) > sys.float_info.epsilon:
+                if not math.isclose(right_subtree.value, 0):
                     self._add_tree(tree_map, tree_queue, left_subtree, right_subtree, "/")
+
+        # If there is no solution, return best solution
+        if not math.isclose(best.value, self.goal):
+            yield best
 
     def _add_tree(self, tree_map, tree_queue, left, right, operator):
         tree = NumberTree(left, right, operator)
@@ -92,5 +100,9 @@ class NumbersRoundSolver(object):
 if __name__ == "__main__":
     solver = NumbersRoundSolver()
     print(solver.numbers, solver.goal)
-    for sol in solver.solve():
+    start = time.perf_counter()
+    solve = solver.solve()
+    for sol in solve:
         print(sol, "=", sol.value)
+    end = time.perf_counter()
+    print(f"{end - start:0.4f} seconds")
